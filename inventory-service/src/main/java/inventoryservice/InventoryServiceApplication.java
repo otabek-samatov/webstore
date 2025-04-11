e73f66a997b51742713fe0ddcda539e5bb0f921b
Otabek Samatov <otabek.samatov@gmail.com>
2025-04-11 09:45:33 +0500

    xcommit

--- inventory-service/src/main/java/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,19 @@
 package inventoryservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(InventoryServiceApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
+        log.info("Inventory Service Application Started");
+
     }
 
 }
