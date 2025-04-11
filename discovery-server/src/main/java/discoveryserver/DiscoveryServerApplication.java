37745717b9845b53d4264cda997f39358e328ba7
Otabek Samatov <otabek.samatov@gmail.com>
2025-04-11 09:39:49 +0500

    test commit

--- discovery-server/src/main/java/discoveryserver/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/discoveryserver/DiscoveryServerApplication.java
@@ -1,13 +1,18 @@
 package discoveryserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DiscoveryServerApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(DiscoveryServerApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryServerApplication.class, args);
+        log.info("Application started");
     }
 
 }
