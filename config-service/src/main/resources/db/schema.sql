d51417f6de721d5d9e4be32c2c01fc31613caf92
Otabek Samatov <otabek.samatov@gmail.com>
2025-09-30 09:51:26 +0500

    Implementing Order Service

--- config-service/src/main/resources/db/schema.sql
+++ config-service/src/main/resources/db/schema.sql
@@ -2,5 +2,6 @@ create schema if not exists product_schema;
 create schema if not exists inventory_schema;
 create schema if not exists user_schema;
 create schema if not exists cart_schema;
+create schema if not exists order_schema;
 
 
